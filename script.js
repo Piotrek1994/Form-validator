9da9233c98be9b60f35861bf15f2dd5ccb35cd9b
Piorek1994 <pprus94@gmail.com>
2022-04-12 18:56:55 +0200

    Validate email

--- script.js
+++ script.js
@@ -43,14 +43,28 @@ const checkPassword = (pass1, pass2) => {
 
 const checkMail = email => {
 	const re =
-		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
+	if (re.test(email.value)) {
+		clearError(email)
+	} else {
+		showError(email, 'E-mail jest niepoprawny')
+	}
+}
 
-        if(re.test(email.value)){
-            clearError(email)
-        } else {
-            showError(email, 'E-mail jest niepoprawny')
-        }
+const checkErrors = () => {
+	const allInputs = document.querySelectorAll('.form-box')
+	let errorCount = 0
+
+	allInputs.forEach(el => {
+		if (el.classList.contains('error')) {
+			errorCount++
+		}
+	})
+
+	if (errorCount === 0) {
+		popup.classList.add('show-popup')
+	}
 }
 
 sendBtn.addEventListener('click', e => {
@@ -60,7 +74,8 @@ sendBtn.addEventListener('click', e => {
 	checkLength(username, 3)
 	checkLength(pass, 8)
 	checkPassword(pass, pass2)
-    checkMail(email)
+	checkMail(email)
+	checkErrors()
 })
 
 clearBtn.addEventListener('click', e => {
@@ -69,5 +84,6 @@ clearBtn.addEventListener('click', e => {
 	const arr = [username, pass, pass2, email]
 	arr.forEach(el => {
 		el.value = ''
+		clearError(el)
 	})
 })
