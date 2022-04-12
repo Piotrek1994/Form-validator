5cad78e2f088f2eaf51b8482583d08df9fd059d6
Piorek1994 <pprus94@gmail.com>
2022-04-12 18:38:48 +0200

    Adding regex in mail

--- script.js
+++ script.js
@@ -29,10 +29,38 @@ const checkForm = input => {
 	})
 }
 
+const checkLength = (input, min) => {
+	if (input.value.length < min) {
+		showError(input, `${input.previousElementSibling.innerText.slice(0, -1)} składa się z min. ${min} znaków.`)
+	}
+}
+
+const checkPassword = (pass1, pass2) => {
+	if (pass1.value !== pass2.value) {
+		showError(pass2, 'Hasła nie są identyczne')
+	}
+}
+
+const checkMail = email => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+        if(re.test(email.value)){
+            clearError(email)
+        } else {
+            showError(email, 'E-mail jest niepoprawny')
+        }
+}
+
 sendBtn.addEventListener('click', e => {
 	e.preventDefault()
 
 	checkForm([username, pass, pass2, email])
+	checkLength(username, 3)
+	checkLength(pass, 8)
+	checkPassword(pass, pass2)
+    checkMail(email)
 })
 
 clearBtn.addEventListener('click', e => {
